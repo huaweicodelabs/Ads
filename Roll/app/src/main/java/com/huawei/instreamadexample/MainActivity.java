dc1a6b91a67d13a802d82fcf0491295779cce782
chenqiuwei <56810797+chenqiuwei@users.noreply.github.com>
2020-12-08 14:34:11 +0800

    Dynamically adding InstreamView

--- Roll/app/src/main/java/com/huawei/instreamadexample/MainActivity.java
+++ Roll/app/src/main/java/com/huawei/instreamadexample/MainActivity.java
@@ -122,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onMediaCompletion(int playTime) {
             updateCountDown(playTime);
+            removeInstream();
             playVideo();
         }
 
